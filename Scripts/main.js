9980baebf00f8dee69a6edb93c156446616cbdb8
JR-Design <47480825+JR-Design@users.noreply.github.com>
2021-03-28 18:10:09 -0400

    NAV & RSVP Form Update
    I added multiple form input fields, spiced up the buttons, added JS to the side nav and took out the trash.

--- Scripts/main.js
+++ Scripts/main.js
@@ -40,13 +40,125 @@ function backPage3() {
 }
 
 function nextPage4() {
-    step4.style.display = 'none';
-    step5.style.display = 'grid';
-}
+        setTimeout(function(){
+
+        step4.style.display = 'none';
+        step5.style.display = 'grid';
+    },2000);
+};
 
 function backPage4() {
     step4.style.display = 'grid';
     step5.style.display = 'none';
 }
 
+
+//FUNCTION for left-side NAV Progress bar indicator ########################
+function pageDepth() {
+    let ScrollTop = document.body.scrollTop || document.documentElement.scrollTop;
+    let ViewportHeight = window.innerHeight;
+    let DocumentHeight = document.body.clientHeight;
+    let progressbarUpdate = (ScrollTop / (DocumentHeight - ViewportHeight)) * 50 + "%";
+    console.log(progressbarUpdate);
+    let x = document.getElementById("progressbar");  
+    x.style.top = progressbarUpdate;
+
+  }
+
+
+
+//JAVASCRIPT FOR CONFIRM BUTTON ################################
+
+gsap.registerPlugin(Physics2DPlugin);
+
+document.querySelectorAll('.button').forEach(button => {
+
+    const bounding = button.getBoundingClientRect();
+
+    button.addEventListener('mousemove', e => {
+
+        let dy = (e.clientY - bounding.top - bounding.height / 2) / -1;
+        let dx = (e.clientX - bounding.left - bounding.width / 2)  / 10;
+
+        dy = dy > 10 ? 10 : (dy < -10 ? -10 : dy);
+        dx = dx > 4 ? 4 : (dx < -4 ? -4 : dx);
+
+        button.style.setProperty('--rx', dy);
+        button.style.setProperty('--ry', dx);
+
+    });
+
+    button.addEventListener('mouseleave', e => {
+
+        button.style.setProperty('--rx', 0);
+        button.style.setProperty('--ry', 0);
+
+    });
+
+    button.addEventListener('click', e => {
+        button.classList.add('success');
+        gsap.to(button, {
+            '--icon-x': -3,
+            '--icon-y': 3,
+            '--z-before': 0,
+            duration: .2,
+            onComplete() {
+                particles(button.querySelector('.emitter'), 100, -4, 6, -80, -50);
+                gsap.to(button, {
+                    '--icon-x': 0,
+                    '--icon-y': 0,
+                    '--z-before': -6,
+                    duration: 1,
+                    ease: 'elastic.out(1, .5)',
+                    onComplete() {
+                        button.classList.remove('success');
+                    }
+                });
+            }
+        });
+    });
+
+});
+
+function particles(parent, quantity, x, y, minAngle, maxAngle) {
+    let colors = [
+        '#FFFF04',
+        '#EA4C89',
+        '#892AB8',
+        '#4AF2FD',
+    ];
+    for(let i = quantity - 1; i >= 0; i--) {
+        let angle = gsap.utils.random(minAngle, maxAngle),
+            velocity = gsap.utils.random(70, 140),
+            dot = document.createElement('div');
+        dot.style.setProperty('--b', colors[Math.floor(gsap.utils.random(0, 4))]);
+        parent.appendChild(dot);
+        gsap.set(dot, {
+            opacity: 0,
+            x: x,
+            y: y,
+            scale: gsap.utils.random(.4, .7)
+        });
+        gsap.timeline({
+            onComplete() {
+                dot.remove();
+            }
+        }).to(dot, {
+            duration: .05,
+            opacity: 1
+        }, 0).to(dot, {
+            duration: 1.8,
+            rotationX: `-=${gsap.utils.random(720, 1440)}`,
+            rotationZ: `+=${gsap.utils.random(720, 1440)}`,
+            physics2D: {
+                angle: angle,
+                velocity: velocity,
+                gravity: 120
+            }
+        }, 0).to(dot, {
+            duration: 1,
+            opacity: 0
+        }, .8);
+    }
+}
 
