3aec9de0ec6393c64afc7c50e261bc0f84d5e3ba
matthewbacon4 <matthewbacon4@gmail.com>
2021-04-01 18:23:00 -0400

    b

--- Scripts/sidenav.js
+++ Scripts/sidenav.js
@@ -12,14 +12,3 @@ function myFunction() {
       x.style.display = "block";
     }
   }
-  function nextStep() {
-    if (document.getElementById("step1")) {
-      if (document.getElementById("step1").style.display == "none") {
-        document.getElementById("step1").style.display = "block";
-        document.getElementById("step2").style.display = "none";
-      } else {
-        document.getElementById("step1").style.display = "none";
-        document.getElementById("step2").style.display = "block";
-      }
-    }
-  }
