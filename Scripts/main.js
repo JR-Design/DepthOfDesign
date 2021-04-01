63457b390b200fd6826a5bedce7ba2c3ef713b86
JR-Design <47480825+JR-Design@users.noreply.github.com>
2021-04-01 17:58:39 -0400

    Update

--- Scripts/main.js
+++ Scripts/main.js
@@ -62,7 +62,34 @@ function pageDepth() {
     let x = document.getElementById("progressbar");  
     x.style.top = progressbarUpdate;
 
+    /*
+    var progressBarTop = parseInt(window.getComputedStyle(
+        document.getElementById('progressbar')).top, 10);
+
+    var progressBarContent2 = document.getElementById('progressBarNumber');
+    
+    
+    var windowHeight = window.innerHeight;
+    var scrollProgress = (progressBarTop/windowHeight)*2;
+
+
+    if (scrollProgress <= 0.15) {
+        progressBarContent2.textContent = "01"
+    } else if ((0.15 < scrollProgress) && (scrollProgress <= 0.4)) {
+        progressBarContent2.textContent = "02"
+    } else if ((0.4 < scrollProgress) && (scrollProgress <= 0.65)) {
+        progressBarContent2.textContent = "03"
+    } else if (0.75 < scrollProgress) {
+        progressBarContent2.textContent = "04"
+    }
+    */
   }
+
+//Function for left side content element
+//const progressbar = document.getElementById("progressbar");
+//console.log(progressbar.style);
+
+
 
 
 
@@ -160,4 +187,5 @@ function particles(parent, quantity, x, y, minAngle, maxAngle) {
         }, .8);
     }
 }
+
 
